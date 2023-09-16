1a811f2fbe2d60b54cb32b0eb4af35c0db08dd76
Lina Beltrán <lmbd92@hotmail.com>
2023-09-16 17:26:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mymlpackage",
-    version='{{VERSION_PLACEHOLDER}}',
+    version="0.1",
     author="Lina Beltrán",
     author_email="linam.beltran@udea.edu.co",
     description="A collection of minimal and clean implementations of machine learning algorithms.",
