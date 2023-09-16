60a443eb9ff9beb305ca3820dc31277fc75b027e
Lina Beltrán <lmbd92@hotmail.com>
2023-09-16 16:31:13 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mymlpackage",
+    version="0.1",
+    author="Lina Beltrán",
+    author_email="linam.beltran@udea.edu.co",
+    description="A collection of minimal and clean implementations of machine learning algorithms.",
+    url="https://github.com/lmbd92/ML_II_UdeA/tree/main/Taller2/mymlpackage",
+    license="MIT",
+    packages=["mymlpackage"],
+    zip_safe=False,
+)
